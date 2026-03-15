b0712e74450edf82581ac661c498b56189d6dd52
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-03-16 00:39:03 +0530

    hy

---
+++ indexing & slicing/access.py
@@ -0,0 +1,12 @@
+"""
+array[index] #1d arry 
+array[row, column] # multidimention array 
+"""
+
+import numpy as np 
+
+arr=np.array([10,20,30,50])
+
+print(arr[1])
+print(arr[0])
+print(arr[-1])
---
+++ indexing & slicing/slicing.py
@@ -0,0 +1,14 @@
+'''
+slicing
+array[start:stop:step]
+
+'''
+import numpy as np
+
+arr=np.array([10,20,30,66,87,22])
+print(arr[::-1])
+print(arr[1:5])
+print(arr[:4])
+print(arr[::2])
+print(arr[1:1:1])
+
