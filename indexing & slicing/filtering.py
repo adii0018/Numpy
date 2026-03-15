811fe324971412745e6d4496745c24283897b4ae
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-03-16 00:48:41 +0530

    yeeh

---
+++ indexing & slicing/filtering.py
@@ -0,0 +1,5 @@
+# isme haam condition lagate he or agar vo condion bollen trure hue toh vo elemnt print hoga verna false wale print nhi honge..
+import numpy as np 
+
+arr=np.array([10,20,30,50])
+print(arr[arr>40])
