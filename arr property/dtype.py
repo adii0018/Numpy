c6bd95c33690b822bf5af436e49612e7681b86cf
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-03-15 23:41:14 +0530

    dtype

---
+++ arr property/dtype.py
@@ -0,0 +1,10 @@
+import numpy as np 
+
+integer=np.array([11,21,1])
+floatt=np.array([11,21,1.21])
+string=np.array(["aa","wd","12","@"])
+
+print(integer.dtype)
+print(floatt.dtype)
+print(string.dtype)
+
