af16411938bb96a3d178118aaa9ee6920694c076
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-03-17 22:00:28 +0530

    m

--- Advanced numpy/spliting.py
+++ Advanced numpy/spliting.py
@@ -3,7 +3,8 @@
 
 import numpy as np 
 
-arr=np.array([10,2,3,4,5,6,7,8])
-
-print(np.split(arr,2))
-print(np.split(arr,2))
+arr=np.array([[10,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8]])
+a1=np.hsplit(arr,2)
+a2=np.vsplit(arr,2)
+print(a1)
+print(a2)
