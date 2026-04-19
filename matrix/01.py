6af2b4b9afa412d6779e844728f51a63d8c6aa6a
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-04-19 21:57:56 +0530

    feat: Add matrix operations examples

---
+++ matrix/01.py
@@ -0,0 +1,10 @@
+import numpy as np 
+a=np.array([[1,2,3],
+            [2,5,6]])
+b=np.array([[1,25,4],
+            [9,22,8]])
+
+print(a+b)
+print(a-b)
+print(a%b)
+print(b/a)
---
+++ matrix/02.py
@@ -0,0 +1,6 @@
+import numpy as np 
+a=np.array([[1,2,3],
+            [2,5,6]])
+b=np.array([1,25,4])
+
+print(a@b)
