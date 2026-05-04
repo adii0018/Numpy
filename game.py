9b96251b327d37a5f78e7da2683e8a2b9da5983b
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-05-04 23:04:44 +0530

    game

---
+++ game.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 800))
+pygame.display.set_caption("My First Game")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))  # black screen
+    pygame.display.update()
+
+pygame.quit()
